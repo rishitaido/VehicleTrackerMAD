0c8d6b94a8ee5eb5549d414e2b7c66c4f3395404
rishitaido <rishitaido@gmail.com>
2025-10-23 22:43:43 -0400

    Typos and file names changes

--- lib/screens/garage_screen.dart
+++ lib/screens/garage_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import '../models/models.dart';
-import '../widgets.dart';
+import '../utility/widgets.dart';
 import '../repos.dart';
 import '../models.dart';
 
